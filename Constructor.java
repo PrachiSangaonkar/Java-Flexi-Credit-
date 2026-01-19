b1571d2d1333300a3cf0e956e0aee7eb7ed1d828
PrachiSangaonkar <prachidsangaonkar@gmail.com>
2026-01-19 19:28:22 +0530

    day-3

---
+++ Constructor.java
@@ -0,0 +1,29 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    /*
+     * Student() {
+     * name = "Prachi";
+     * age = 19;
+     * }
+     */
+    void display() {
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        // Student s = new Student();
+        Student s = new Student("Prachi", 19);
+        s.display();
+    }
+
+}
